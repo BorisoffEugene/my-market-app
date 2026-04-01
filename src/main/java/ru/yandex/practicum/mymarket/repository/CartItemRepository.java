100c231f55fdad7c8c0a4e47672cdf1baaec8b35
BorisoffEugene <Borisoff_Eugene@mail.ru>
2026-04-01 17:31:57 +0300

    Репозитории

--- src/main/java/ru/yandex/practicum/mymarket/repository/CartItemRepository.java
+++ src/main/java/ru/yandex/practicum/mymarket/repository/CartItemRepository.java
@@ -1,27 +1,25 @@
 package ru.yandex.practicum.mymarket.repository;
 
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
+
+import org.springframework.data.r2dbc.repository.Query;
+import org.springframework.data.repository.reactive.ReactiveCrudRepository;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 import ru.yandex.practicum.mymarket.domain.CartItem;
 import ru.yandex.practicum.mymarket.domain.Item;
 
-import java.util.List;
-import java.util.Optional;
-
-public interface CartItemRepository extends JpaRepository<CartItem, Long> {
-    Optional<CartItem> findByItemId(Long itemId);
-    @Query(
-            nativeQuery = true,
-            value = """
-                select
-                    i.id, i.title, i.description, i.img_path, i.price, ci.count 
-                from 
-                    market.cart c
-                    join market.cart_items ci on ci.cart_id = c.id
-                    join market.items i on i.id = ci.item_id
-                where
-                    c.status = 'CURRENT'
-                """
+public interface CartItemRepository extends ReactiveCrudRepository<CartItem, Long> {
+    Mono<CartItem> findByItemId(Long itemId);
+    @Query("""
+                    select
+                        i.id, i.title, i.description, i.img_path, i.price, ci.count
+                    from
+                        market.cart c
+                        join market.cart_items ci on ci.cart_id = c.id
+                        join market.items i on i.id = ci.item_id
+                    where
+                        c.status = 'CURRENT'
+                    """
     )
-    List<Item> findCartItems();
+    Flux<Item> findCartItems();
 }
--- src/main/java/ru/yandex/practicum/mymarket/repository/ItemRepository.java
+++ src/main/java/ru/yandex/practicum/mymarket/repository/ItemRepository.java
@@ -1,17 +1,15 @@
 package ru.yandex.practicum.mymarket.repository;
 
-import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
-import org.springframework.data.jpa.repository.*;
+import org.springframework.data.r2dbc.repository.Query;
+import org.springframework.data.r2dbc.repository.R2dbcRepository;
 import org.springframework.data.repository.query.Param;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 import ru.yandex.practicum.mymarket.domain.Item;
 
-import java.util.Optional;
-
-public interface ItemRepository extends JpaRepository<Item, Long> {
-    @Query(
-            nativeQuery = true,
-            value = """
+public interface ItemRepository extends R2dbcRepository<Item, Long> {
+    @Query("""
                     select
                     	i.id, i.title, i.description, i.price, i.img_path, coalesce(ci.count, 0) count
                     from
@@ -20,21 +18,11 @@ public interface ItemRepository extends JpaRepository<Item, Long> {
                     	left join market.cart_items ci on ci.cart_id = c.id and ci.item_id = i.id
                     where
                     	i.title ilike '%'||:search||'%' or i.description ilike '%'||:search||'%'
-                    """,
-            countQuery = """
-                    select
-                    	count(i.id)
-                    from
-                    	market.items i
-                    where
-                    	i.title ilike '%'||:search||'%' or i.description ilike '%'||:search||'%'
                     """
     )
-    Page<Item> findByFiltr(@Param("search") String search, Pageable pageable);
+    Flux<Item> findByFiltr(String search, Pageable pageable);
 
-    @Query(
-            nativeQuery = true,
-            value = """
+    @Query("""
                     select
                     	i.id, i.title, i.description, i.price, i.img_path, coalesce(ci.count, 0) count
                     from
@@ -45,5 +33,5 @@ public interface ItemRepository extends JpaRepository<Item, Long> {
                     	i.id = :id
                     """
     )
-    Optional<Item> findById(@Param("id") Long id);
+    Mono<Item> findById(@Param("id") Long id);
 }
